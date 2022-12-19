cbd5953d90d8188477097a9b8f8eb60625f008f9
Atinderbir Singh <atinderbir3@gmail.com>
2022-12-19 16:52:36 +0530

    Fixed an issue in bookEdit function

--- src/App.js
+++ src/App.js
@@ -4,42 +4,44 @@ import BookList from "./components/BookList";
 import "./App.css";
 import api from "./api";
 
-
 export default function App() {
   const [books, setBooks] = useState([]);
 
   async function getBooks() {
-    const data = await api.get('http://localhost:3001/books',);
+    const data = await api.get("http://localhost:3001/books");
     setBooks(data);
   }
 
   useEffect(() => {
-    getBooks()
-  },[])
+    getBooks();
+  }, []);
 
   async function createBook(title) {
-    const newBook = await api.post('http://localhost:3001/books/',title);
+    const newBook = await api.post("http://localhost:3001/books/", title);
     setBooks([...books, newBook]);
   }
 
   async function deleteBook(index) {
     const response = await api.remove(`http://localhost:3001/books/${index}`);
     if (response !== null) {
-      const updatedBooks = books.filter(book => book.id !== index);
+      const updatedBooks = books.filter((book) => book.id !== index);
       setBooks(updatedBooks);
     }
   }
 
   async function editBook(index, newTitle) {
-    const response = await api.put(`http://localhost:3001/books/${index}`,newTitle);
+    const response = await api.put(
+      `http://localhost:3001/books/${index}`,
+      { title: newTitle },
+    );
 
     if (response) {
-      const updatedBooks = books.map(book => {
-          if(book.id === index) {
-              return { ...book, title:newTitle };
-          }
-  
-          return book;
+      const updatedBooks = books.map((book) => {
+        if (book.id === index) {
+          return { ...book, ...response };
+        }
+
+        return book;
       });
       setBooks(updatedBooks);
     }
@@ -47,7 +49,11 @@ export default function App() {
 
   return (
     <div className="flex flex-col">
-      <BookList books={books} handleDeleteBook={deleteBook} handleEditBook={editBook} />
+      <BookList
+        books={books}
+        handleDeleteBook={deleteBook}
+        handleEditBook={editBook}
+      />
       <BookCreate handleCreateBook={createBook} />
     </div>
   );
