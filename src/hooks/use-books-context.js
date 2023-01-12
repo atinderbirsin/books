90377d96d05172ca4ceaf3dd09e6711ffa5600df
Atinderbir Singh <atinderbir3@gmail.com>
2023-01-12 15:04:15 +0530

    used useDebugValue to inspect value returned by hook context

--- src/hooks/use-books-context.js
+++ src/hooks/use-books-context.js
@@ -1,7 +1,11 @@
-import { useContext } from "react";
+import { useContext, useDebugValue } from "react";
 import BooksContext from "../context/books";
 
 function useBooksContext() {
+    const { books } = useContext(BooksContext);
+
+    useDebugValue(books, () => books.map(book => book.title));
+
     return useContext(BooksContext);
 }
 
